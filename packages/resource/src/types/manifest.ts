3de7c650c172572b1cdb134ddd6d1a9773f93792
wanxger <wanxger@egeeke.com>
2025-04-08 00:40:18 +0000

    feat: resource

--- packages/resource/src/types/manifest.ts
+++ packages/resource/src/types/manifest.ts
@@ -1,5 +1,48 @@
-import { Resource } from "./resource";
+export namespace Manifest {
+  /**
+   * Resource Settings
+   */
+  export interface ResourceSettings {
+    /**
+     * Resource Title
+     */
+    title: string;
 
+    /**
+     * Resource Description
+     */
+    description: string;
+  }
+
+  /**
+   * Resource
+   */
+  export interface Resource<T extends ResourceSettings = ResourceSettings> {
+    /**
+     * Resource ID
+     */
+    id: string;
+
+    /**
+     * Resource Kind
+     */
+    kind: string;
+
+    /**
+     * Parent Resource ID
+     */
+    parentId: string;
+
+    /**
+     * Resource Settings
+     */
+    settings: T;
+  }
+}
+
+/**
+ * Manifest
+ */
 export interface Manifest {
-    resources: Resource[]
+  resources: Manifest.Resource[];
 }
