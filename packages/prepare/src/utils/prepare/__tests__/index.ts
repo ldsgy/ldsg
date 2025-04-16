995d592fa0a0ac73ad25f1bc938af10806848801
wanxger <wanxger@egeeke.com>
2025-04-16 11:29:03 +0000

    feat: fix

--- packages/prepare/src/utils/prepare/__tests__/index.ts
+++ packages/prepare/src/utils/prepare/__tests__/index.ts
@@ -2,7 +2,7 @@ import path from "path";
 import { prepare } from "..";
 import MANIFEST_JSON from "./manifest.json";
 
-describe("prepare", async () => {
+test("prepare", async () => {
   await prepare({
     ...MANIFEST_JSON,
     outputPath: path.join(__dirname, "apps", "main"),
