a8284e3b13056d39184be8b4b9857bfd76eb2d8a
wanxger <wanxger@egeeke.com>
2025-04-17 00:25:01 +0000

    feat: manifest-resolver

--- packages/manifest-resolver/src/utils/index.ts
+++ packages/manifest-resolver/src/utils/index.ts
@@ -1,3 +1,2 @@
-export * from "./resolve-manifest";
 export * from "./resolve-manifest-resources";
 export * from "./resolve-manifest-resources-of-type-resource-definition";
--- packages/manifest-resolver/src/utils/resolve-manifest-resources-of-type-resource-definition.spec.ts
+++ packages/manifest-resolver/src/utils/resolve-manifest-resources-of-type-resource-definition.spec.ts
@@ -1,36 +1,13 @@
-import { ApplicationResource } from "@ldsg/application";
 import { appManifest } from "@ldsg/constants";
-import request from "supertest";
 import { resolveManifestResourcesOfTypeResourceDefinition } from "./resolve-manifest-resources-of-type-resource-definition";
 
-test("resolve-manifest-resources", async () => {
+test("resolve-manifest-resources", () => {
   const { resources } = appManifest;
 
-  const { kindResourceMap } = resolveManifestResourcesOfTypeResourceDefinition({
-    manifestResources: resources,
-  });
+  const { kindResourceDefinitionResourceMap } =
+    resolveManifestResourcesOfTypeResourceDefinition({
+      manifestResources: resources,
+    });
 
-  const { resource: applicationResource } = kindResourceMap[
-    "APPLICATION"
-  ].instantiateResource({
-    resourceConstructorParams: {
-      id: "main-app",
-      kind: "APPLICATION",
-      parentId: "ROOT",
-      settings: {
-        title: "主要应用",
-        description: "",
-      },
-    },
-  });
-
-  console.debug("applicationResource", applicationResource);
-
-  const app = (applicationResource as ApplicationResource).createApp();
-
-  const response = await request(app).get("/");
-
-  expect(response.statusCode).toBe(200);
-
-  expect(response.text).toBe("Hello, World!");
+  expect(kindResourceDefinitionResourceMap).toMatchSnapshot();
 });
