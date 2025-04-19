eaf5fbfbf49260d6fcc5b2bb6565a9f100f7f348
wanxger <wanxger@egeeke.com>
2025-04-19 17:40:55 +0000

    feat: test

--- packages/integration-test/src/__tests__/index.ts
+++ packages/integration-test/src/__tests__/index.ts
@@ -1,9 +1,11 @@
-import { APP_MANIFEST } from "@ldsg/constants";
+import { APP_MANIFEST, FILES_IN_ROOT_MODULE } from "@ldsg/constants";
 import { prepare } from "@ldsg/prepare";
 import { extendAppData } from "@ldsg/utils";
 import fs from "fs-extra";
 import path from "path";
 import shell from "shelljs";
+import request from "supertest";
+import { app } from "../../apps/main/dist";
 
 const appsDirPath = path.join(__dirname, "..", "..", "apps");
 
@@ -11,9 +13,7 @@ const currentAppDirPath = path.join(appsDirPath, "main");
 
 beforeEach(async () => {
   await fs.rm(currentAppDirPath, { force: true, recursive: true });
-});
 
-test("prepare", async () => {
   const appData = extendAppData({
     appData: APP_MANIFEST,
     extraAppData: {
@@ -21,6 +21,21 @@ test("prepare", async () => {
         "graphql",
         "graphql-middleware",
         "graphql-shield",
+      ],
+      filesInRootModele: [
+        ...FILES_IN_ROOT_MODULE,
+        {
+          path: "index.ts",
+          data: `import { createApp } from "@ldsg/app";
+import MANIFEST_JSON from "./manifest.json";
+
+const { resourceRecords } = MANIFEST_JSON;
+
+export const app = createApp({
+  resourceRecords,
+});
+`,
+        },
       ],
     },
   });
@@ -35,8 +50,12 @@ test("prepare", async () => {
   shell.exec("pnpm i");
 
   shell.exec("pnpm build");
+});
 
-  /**
-   * TODO: need to test pnpm start
-   */
+test("prepare", async () => {
+  const response = await request(app).get("/");
+
+  expect(response.statusCode).toBe(200);
+
+  expect(response.text).toBe("Hello, World!");
 });
