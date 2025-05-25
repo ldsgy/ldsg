d11c17120f1cd9374dbb3b021a7bded98bafc905
wanxger <wanxger@egeeke.com>
2025-05-25 12:43:58 +0000

    feat: test

--- test/__tests__/e2e/blog.ts
+++ test/__tests__/e2e/blog.ts
@@ -1,5 +1,7 @@
 import { createApp } from "@ldsg/create-app";
+import { RESOURCE_DEFINITION_SPECIFIC_RESOURCE_SETTINGS as HANDLER_RESOURCE_DEFINITION_SPECIFIC_RESOURCE_SETTINGS } from "@ldsg/handler";
 import request from "supertest";
+import {} from "typescript";
 import { blogResourceRecords as resourceRecords } from "../../apps";
 
 describe("blog", () => {
@@ -7,9 +9,48 @@ describe("blog", () => {
     expect(resourceRecords).toMatchSnapshot();
   });
 
+  const resourceRecordsWithHandlerFunction = resourceRecords.map(
+    (resourceRecord) => {
+      const { id, kind, settings } = resourceRecord;
+
+      let res = resourceRecord;
+
+      switch (kind) {
+        case HANDLER_RESOURCE_DEFINITION_SPECIFIC_RESOURCE_SETTINGS.kind: {
+          const { code: tsCode, handler: settingsHandler } = settings;
+
+          console.debug("id", id);
+
+          console.debug("tsCode", tsCode);
+
+          const jsCode = tsCode;
+
+          const handler = new Function(jsCode);
+
+          if (!settingsHandler) {
+            res = {
+              ...resourceRecord,
+              settings: {
+                ...settings,
+                handler,
+              },
+            };
+          }
+
+          break;
+        }
+
+        default:
+          break;
+      }
+
+      return res;
+    }
+  );
+
   describe("app", () => {
     const app = createApp({
-      resourceRecords,
+      resourceRecords: resourceRecordsWithHandlerFunction,
     });
 
     test("is health", async () => {
